1b4313b7bdf5ae897b51c3d07c793cf92e4289f9
Acanthiza <nigel.willoughby@sa.gov.au>
2021-10-27 10:39:27 +1030

    added `make_ll_model`

--- R/make_ll_model.R
+++ R/make_ll_model.R
@@ -1,6 +1,6 @@
 
 
-#' Make, and save to disk, a reporting rate model.
+#' Make, and save to disk, a list-length corrected reporting rate model.
 #'
 #' @param taxa Character name of taxa for which model is being run. Used to name
 #' output file.
@@ -10,24 +10,24 @@
 #' @param random_col Character name of column in `df` containing random factor
 #' for model. This is usually the larger of two (probably raster) grid cell
 #' sizes.
-#' @param out_path Path into which model results are saved.
+#' @param out_path Directory into which model results are saved.
 #' @param ... Passed to rstanarm::stan_gamm4 (e.g. chains, iter)
 #'
-#' @return `fs::path(out_path,paste0("reporting-rate_mod_",taxa,".rds"))`
+#' @return `fs::path(out_path,paste0("list-length_mod_",taxa,".rds"))`
 #' @export
 #'
 #' @examples
-make_rr_model <- function(taxa
-               , df
-               , geo_cols
-               , random_col = "grid_l"
-               , out_path
-               , ...
-               ) {
+make_ll_model <- function(taxa
+                          , df
+                          , geo_cols
+                          , random_col = "grid_l"
+                          , out_path
+                          , ...
+                          ) {
 
   print(taxa)
 
-  out_file <- fs::path(out_path,paste0("reporting-rate_mod_",taxa,".rds"))
+  out_file <- fs::path(outDir,paste0("list-length_mod_",taxa,".rds"))
 
   geos <- df %>%
     dplyr::distinct(across(any_of(geo_levels))) %>%
@@ -44,10 +44,14 @@ make_rr_model <- function(taxa
 
     mod <- rstanarm::stan_gamm4(as.formula(paste0("cbind(success,trials - success) ~ "
                                                   , "s(year, k = 4, bs = 'ts') +"
+                                                  , "s(year, k = 4, by = "
                                                   , geo2
-                                                  , " + s(year, k = 4, by = "
+                                                  , ", bs = 'ts') +"
+                                                  , "s(year, k = 4, by = log_list_length, bs = 'ts') + "
                                                   , geo2
-                                                  , ", bs = 'ts')"
+                                                  , "+"
+                                                  , geo2
+                                                  , "*log_list_length"
                                                   )
                                            )
                                 , data = df
@@ -62,25 +66,25 @@ make_rr_model <- function(taxa
 
   } else {
 
-    mod <- stan_gamm4(cbind(success, trials-success) ~ s(year, k = 4, bs = "ts")
+    mod <- stan_gamm4(cbind(success,trials-success) ~ s(year, k = 4, bs = "ts") +
+                        s(year, k = 4, by = log_list_length, bs = "ts") +
+                        log_list_length
                       , data = df
                       , random = if(grid_cells > 1) {
 
-                          as.formula(paste0("~(1|"
-                                            , random_col
-                                            , ")"
-                                            )
-                                     )
+                        as.formula(paste0("~(1|"
+                                          , random_col
+                                          , ")"
+                                          )
+                                   )
 
                         } else NULL
                       , family = stats::binomial()
                       , ...
                       )
 
-    }
+  }
 
-  rio::export(mod
-              , out_file
-              )
+  write_rds(mod,out_file)
 
 }
