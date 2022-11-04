69d92389413668d39a4b105bf58e1a8f0dc8c83d
Acanthiza <nigel.willoughby@sa.gov.au>
2022-11-04 13:51:52 +1030

    Fixed lulikelihood documentation format issues

--- R/data.R
+++ R/data.R
@@ -7,12 +7,24 @@
 #' `r ncol(lulikelihood)` variables:
 #' \describe{
 #'   \item{likelihood}{Factor. Definitions.}
+#'
 #'   \item{maxVal}{Double. Maximum proportion to fall in likelihood category}
-#'   \item{range}{Factor. Result of `cut(lulikelihood$maxVal, breaks = c(0,lulikelihood$maxVal)) %>% envFunc::vec_to_sentence()`}
-#'   \item{loose}{Factor with levels: `r levels(lulikelihood$loose) %>% envFunc::vec_to_sentence()`}
-#'   \item{very}{Factor with levels: `r levels(lulikelihood$very) %>% envFunc::vec_to_sentence()`}
-#'   \item{extreme}{Factor with levels: `r levels(lulikelihood$extreme) %>% envFunc::vec_to_sentence()`}
-#'   \item{exceptional}{Factor with levels: `r levels(lulikelihood$exceptional) %>% envFunc::vec_to_sentence()`}
+#'
+#'   \item{range}{Factor. Result of
+#'   `cut(lulikelihood$maxVal, breaks = c(0,lulikelihood$maxVal))`}
+#'
+#'   \item{loose}{Factor with levels:
+#'   `r levels(lulikelihood$loose) %>% envFunc::vec_to_sentence()`}
+#'
+#'   \item{very}{Factor with levels:
+#'   `r levels(lulikelihood$very) %>% envFunc::vec_to_sentence()`}
+#'
+#'   \item{extreme}{Factor with levels:
+#'   `r levels(lulikelihood$extreme) %>% envFunc::vec_to_sentence()`}
+#'
+#'   \item{exceptional}{Factor with levels:
+#'   `r levels(lulikelihood$exceptional) %>% envFunc::vec_to_sentence()`}
+#'
 #'   ...
 #' }
 #' @source <https://www.ipcc.ch/site/assets/uploads/2017/08/AR5_Uncertainty_Guidance_Note.pdf>
